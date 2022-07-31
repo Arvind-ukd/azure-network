d1e8300e565cc51a036d1e38a7b1187763eaf096
arvindng88 <arvindng88@gmail.com>
2022-07-31 19:13:22 +0530

    n

--- main.tf
+++ main.tf
@@ -9,7 +9,9 @@ resource "azurerm_virtual_network" "example" {
     name           = "india-subnet"
     address_prefix = "10.0.1.0/24"
   }
+  }
 
+resource "azurerm_virtual_network" "example" {
   name                = "eastasia-network"
   location            =  "East Asia"
   resource_group_name = data.azurerm_resources.example.resources_group_name
@@ -20,7 +22,9 @@ resource "azurerm_virtual_network" "example" {
     name           = "eastasia-subnet"
     address_prefix = "192.168.0.0/24"
   }
+  }
 
+resource "azurerm_virtual_network" "example" {
   name                = "southindia-network"
   location            =  "South India"
   resource_group_name = data.azurerm_resources.example.resources_group_name
@@ -31,6 +35,8 @@ resource "azurerm_virtual_network" "example" {
     name           = "eastasia-subnet"
     address_prefix = "172.16.0.0/24"
   }
+  }
+  resource "azurerm_virtual_network" "example" {
 
   name                = "ukwest-network"
   location            =  "UK West"
@@ -41,4 +47,5 @@ resource "azurerm_virtual_network" "example" {
   subnet {
     name           = "ukwest-subnet"
     address_prefix = "10.10.0.0/24"
+  }
   }
